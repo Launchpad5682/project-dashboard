24646dbcd96cd674876f495ff58313f95f339cb0
launchpad5682 <launchpad5682@gmail.com>
2023-01-25 16:04:33 +0530

    feat: barrel exports for library added

---
+++ src/library/index.ts
@@ -0,0 +1,16 @@
+// atoms
+export { Avatar } from './atoms/avatar';
+export { Button } from './atoms/button';
+export { Header } from './atoms/header';
+export { Main } from './atoms/main';
+export { Slider } from './atoms/slider';
+export { Switch } from './atoms/switch';
+export { P, H2 } from './atoms/typography';
+
+// molecules
+export { AppContainer } from './molecules/app-container';
+export { ProgressBar } from './molecules/progress-bar';
+
+// charts
+export { LineChart } from './molecules/charts/line-chart';
+export { PieChart } from './molecules/charts/pie-chart';
