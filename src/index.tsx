38d31f7b2a57952d7efc13b81d72b87ae75e2377
launchpad5682 <launchpad5682@gmail.com>
2023-01-25 15:55:30 +0530

    feat: Routes, DataProvider, BrowserRouter added

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { css, Global } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DataProvider } from './contexts/data-context';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Global
+			styles={css`
+				* {
+					padding: 0;
+					margin: 0;
+					font-size: 62.5%;
+					font-family: 'Poppins', sans-serif;
+				}
+			`}
+		/>
+		<BrowserRouter>
+			<DataProvider>
+				<App />
+			</DataProvider>
+		</BrowserRouter>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
