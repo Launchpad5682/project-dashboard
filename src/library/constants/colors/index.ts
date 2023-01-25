7e8073201ad9d15cfdfc591b5a9cc4751ce2c585
launchpad5682 <launchpad5682@gmail.com>
2023-01-25 16:07:16 +0530

    constants: Colors, boxShadow, and maxWidth added

---
+++ src/library/constants/colors/index.ts
@@ -0,0 +1,27 @@
+export const colors = {
+	background: '#F9F9F9',
+	primary: '#2F3549',
+	white: '#FFFFFF',
+	red: {
+		R1: '#E11F46',
+		R2: '#FF768A',
+	},
+	green: {
+		G1: '#007E73',
+		G2: '#C7E6E5',
+	},
+	yellow: {
+		Y1: '#E49000',
+		Y2: '#FFE9B1',
+	},
+	teal: {
+		T1: '#00DBC4',
+	},
+	black: {
+		B1: '#3E3E3E',
+		B2: '#D3D3D3',
+	},
+	violet: {
+		V1: '#7C8AD1',
+	},
+};
---
+++ src/library/constants/index.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+import { colors } from './colors';
+
+export const screenMaxWidth = 1400;
+export const borderRadius = 4;
+
+export const boxShadow = css`
+	box-shadow: 10px 10px 33px -22px ${colors.black.B1};
+`;
