4ee19a5c5f465c7aea841f58e3e3cd6d47f1b683
launchpad5682 <launchpad5682@gmail.com>
2023-01-25 15:58:51 +0530

    feat: dummy data added for data context

---
+++ src/contexts/data-context/data/index.ts
@@ -0,0 +1,51 @@
+import { DataState } from '../types';
+
+export const initialState: DataState = {
+	teamMood: [
+		{
+			id: 'eid4562',
+			name: 'Andrea',
+			department: 'UX Junior',
+			value: 3,
+			mood: '😊',
+			avatarURL:
+				'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		},
+		{
+			id: 'eid4563',
+			name: 'Alvaro',
+			department: 'Backend Developer',
+			value: 10,
+			mood: '😩',
+			avatarURL:
+				'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		},
+		{
+			id: 'eid4564',
+			name: 'Juan',
+			department: 'UX Senior',
+			value: 7,
+			mood: '😊',
+			avatarURL:
+				'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		},
+		{
+			id: 'eid4565',
+			name: 'Jose',
+			department: 'Marketing',
+			value: 1,
+			mood: '😭',
+			avatarURL:
+				'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		},
+		{
+			id: 'eid4566',
+			name: 'Maria',
+			department: 'UX Junior',
+			value: 5,
+			mood: '😩',
+			avatarURL:
+				'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+		},
+	],
+};
